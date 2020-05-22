0276a63d4c2e74a9b9f8644c688109f797cf8b40
jrlvarghese <jrlvarghese@gmail.com>
2020-05-22 23:32:38 +0530

    Add file eduonix_php.php Declaring constants, variables, array in php

---
+++ eduonix_php.php
@@ -0,0 +1,13 @@
+<?php
+/* PHP FILE FOR LEARNING PHP */
+echo "Jeril Varghese";
+// following is the way to define a constant
+define('HOSTNAME', 'localhost');
+echo('<br>'.HOSTNAME);
+// declaring and using variables
+$name = 'jeril varghese';
+echo('<br>'.$name.'<br>');
+// Define an array in php
+$users = Array('jeril', 'ajoy', 'jithin');
+echo '<br>'.$users;
+print_r($users);
