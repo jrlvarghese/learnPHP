f25cf176f49be546ceadf0a4dcf4b5be54e50ddc
jrlvarghese <jrlvarghese@gmail.com>
2020-05-23 23:37:58 +0530

    Add examples with different data types, date and time.

--- eduonix_php.php
+++ eduonix_php.php
@@ -55,3 +55,54 @@ echo '<br><br>Traversing through an array using foreach<br>';
 foreach($users as $user){
     echo $user.'<br>';
 }
+
+// learning variables 
+echo '<br>Learn variables through examples<br>';
+$my_string = 'its a string';
+var_dump($my_string);
+
+echo '<br>';
+$myInt = 10;
+var_dump($myInt);
+
+echo '<br>';
+$myFloat = 8.3;
+var_dump($myFloat);
+
+echo '<br>';
+$myBool = true;
+var_dump($myBool);
+
+echo '<br>';
+$myArray = Array(3,5,5,2);
+var_dump($myArray);
+
+echo '<br>';
+$myNull = null;
+var_dump($myNull);
+
+// strings
+echo '<br>';
+$my_string = "Is a string";
+// getting length of a string using strlen
+echo '<br>length of string using strlen(string) is: '.strlen($my_string);
+// for getting  word count of a string str_word_count
+echo '<br>word count of string using str_word_count(string) is: '.str_word_count($my_string);
+// reversing a string
+$temp = $my_string;
+echo '<br>reversing string using strrev(string): '.strrev($temp);
+// getting position of a particular word in string
+echo "<br>position of 'a' in string using strpos(string, substring): ".strpos($my_string, 'a');
+// replacing a word in string
+echo "<br>replace 'a' with 'not a' using str_replace('a', 'not a', string): ".str_replace('a', 'not a', $my_string);
+
+echo '<br><br>list of functions for numbers: <br>max()<br>min()<br>abs()<br>floor()<br>ceil()<br>rand(a,b)';
+//Date in php
+echo '<br>Date is: '.date('d/m/y').", format used: 'd/m/y'";
+echo '<br>Date is: '.date('d/m/yy').", format used: 'd/m/yy'";
+echo '<br>Day is: '.date('l').", format used: 'l'";
+echo '<br>Month is: '.date('m').", format used: 'm'";
+echo '<br>month is: '.date('M').", format used: 'M'";
+echo '<br>year is: '.date('yy').", format used: 'yy'";
+echo '<br>time is: '.date('h:i:sa').", format used: 'h:i:sa'";
+echo '<br>time is: '.date('d/m/y',strtotime("tomorrow"));
