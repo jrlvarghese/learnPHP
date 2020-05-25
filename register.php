e41214431675b1b84d79745d3c0fc432db7de177
jrlvarghese <jrlvarghese@gmail.com>
2020-05-25 12:43:11 +0530

    Add new file 'register.php'.

---
+++ register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User profile</title>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+</head>
+
+<body>
+    <div class='w3-container w3-margin w3-padding'>
+        <h2>User profile</h2>
+    </div>
+</body>
+</html>
