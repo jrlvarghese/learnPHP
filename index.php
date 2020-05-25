8512e54a792e5f83c75b9e05acec9d3d06e1a253
jrlvarghese <jrlvarghese@gmail.com>
2020-05-25 16:16:26 +0530

    Add cookie variables

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$cookie_name = "username";
+$cookie_value = "jeril";
+//set cookie
+//setcookie($cookie_name, $cookie_value, time()+1800);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cookies using php</title>
+    </head>
+    <body>
+        <?php
+            if(!isset($_COOKIE[$cookie_name])){
+                echo 'Cookie '.$cookie_name.' is not set';
+            }else{
+                echo 'Cookie '.$cookie_name.' is set';
+                echo '<br>Cookie value: '.$cookie_value;
+            }
+        ?>
+    </body>
+</html>
